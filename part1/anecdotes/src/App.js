3b3bf93d732d6872b0dd295857819273799dabec
Seco Vlad <seco.vlad@gmail.com>
2021-09-01 21:21:21 +0300

    [Part 1] ex. 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react'
 
+const GenericButton = ({text, onClick}) => (
+  <div>
+    <button onClick={onClick}>{text}</button>
+  </div>
+);
+
+const AnecdoteDisplay = ({anecdote}) => (
+  <h3>{anecdote}</h3>
+)
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -10,12 +20,21 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
-  const [selected, setSelected] = useState(0)
+
+  const [selected, setSelected] = useState(0);
+
+  const setRandomIndex = () => { 
+    let result = selected;
+    do { // ensure we don't display the same anecdote twice in a row
+      result = Math.floor(Math.random() * anecdotes.length); 
+    } while (result === selected);
+    setSelected(result);
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <AnecdoteDisplay anecdote={anecdotes[selected]} />
+      <GenericButton text="Next Anectode" onClick={setRandomIndex} />
     </div>
   )
 }
