386c6e341f1ecb8e79cbd3163e1bcd9c4e3c57bd
Seco Vlad <seco.vlad@gmail.com>
2021-09-02 13:57:23 +0300

    [Part2] Ex 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -24,7 +24,9 @@ const Content = ({ course }) => {
 
 const Total = ({ course }) => (
   <div>
-    <p>Number of exercises {course.parts.map(el => el.exercises).reduce((a, b) => a + b, 0)}</p>
+    <strong>
+      Total number of exercises = {course.parts.map(el => el.exercises).reduce((a, b) => a + b, 0)}
+    </strong>
   </div>
 )
 
@@ -37,34 +39,76 @@ const Course = ({ course }) => (
 );
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Paul Otlet and his Visions of Xanadu',
-        exercises: 1,
-        id: 4,
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        }
+      ]
+    },
+    {
+      name: 'Foundations of Hypermedia',
+      id: 2,
+      parts: [
+        {
+          name: 'Paul Otlet and his Visions of Xanadu',
+          exercises: 15,
+          id: 1
+        },
+        {
+          name: 'Douglas Engelbart and the Mother of All Demos',
+          exercises: 3,
+          id: 2,
+        },
+        {
+          name: 'Ted Nelson and the Curse of Xanadu',
+          exercises: 12,
+          id: 3,
+        },
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 3,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    },
+  ];
 
   return (
-    <Course course={course} />
+    <div>
+      {courses.map(course => <Course key ={course.id} course={course} />)}
+    </div>
   );
 }
 
