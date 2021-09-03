3fa6bbf2f7ba77f0eb3d46be232162dad6a78e63
Seco Vlad <seco.vlad@gmail.com>
2021-09-03 16:44:48 +0300

    [Part2] Ex 2.14 [buggy]

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,41 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+const CapitalWeatherDisplay = ({ countryCapital }) => {
+  const apiKey = process.env.REACT_APP_WEATHERSTACK_API_KEY;
+  // prevent a strange bug with boilerplate placeholder data
+  const [weatherData, setWeatherData] = useState({
+    temperature: 'placeholder',
+    weather_descriptions: ['placeholder'],
+    weather_icons: ['https://image.shutterstock.com/image-illustration/trollface-laughing-internet-meme-troll-600w-201282305.jpg'],
+    wind_dir: "PH",
+    wind_speed: 'placeholder'
+  });
+
+  // weird bug - sends request twice, first time doesn't work...
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${countryCapital}`)
+      .then(response => { console.log("RESP DATA", response.data.current); setWeatherData(response.data.current); }
+      );
+  }, [apiKey, countryCapital]);
+
+  console.log("weatherData", weatherData);
+
+  // it STILL crashes sometimes - can't figure out what causes it...
+  return (
+    <div>
+      <h2>Weather in {countryCapital}</h2>
+      <p>It is currently <strong>{weatherData.weather_descriptions[0]}</strong></p>
+      <p><strong>Temperature: </strong>{weatherData.temperature}°C</p>
+      <img src={weatherData.weather_icons[0]} alt="Weather Icon" width="240" height="240"></img>
+      <p><strong>Wind: </strong>
+        {weatherData.wind_speed}mph in {weatherData.wind_dir} direction.
+      </p>
+    </div>
+  );
+}
+
 const CountryComponentDisplay = ({ countryData }) => (
   <div>
     <h1>{countryData.name}</h1>
@@ -9,32 +44,33 @@ const CountryComponentDisplay = ({ countryData }) => (
     <h3>Population: {countryData.population.toLocaleString()}</h3>
     <h2>Languages</h2>
     <ul>
-    {countryData.languages.map(language => <li>{language.name}</li>)}
+      {countryData.languages.map(language => <li key={language.name}>{language.name}</li>)}
     </ul>
     <h2>Flag</h2>
-    <img src={countryData.flag} alt="Flag of the Country"/>
+    <img src={countryData.flag} alt="Flag of the Country" className="countryFlag"/>
+    <CapitalWeatherDisplay countryCapital={countryData.capital} />
   </div>
 );
 
 const IAmErrorComponent = () => (
-  <p>One of three things has gone wrong:
+  <div>One of three things has gone wrong:
     <ul>
       <li>The data has not been downloaded yet (or cannot be downloaded - maybe the API is down)</li>
       <li>The country you are searching for does not exist</li>
       <li>I messed something up and failed to account for it</li>
     </ul>
     Hopefully if you wait the program will work as expected. Also, try refreshing the page!
-  </p>
+  </div>
 )
 
-const ShowDataButtonDisplay = ({countryData}) => {
+const ShowDataButtonDisplay = ({ countryData }) => {
   const [dataIsShown, setDataIsShown] = useState(false);
   const showButtonOnClick = () => setDataIsShown(!dataIsShown);
 
   return (
     <span>
-      <button onClick={showButtonOnClick}>{ dataIsShown ? "Hide" : "Show"}</button>
-      {dataIsShown ? <CountryComponentDisplay countryData={countryData}/> : <span />}
+      <button onClick={showButtonOnClick}>{dataIsShown ? "Hide" : "Show"}</button>
+      {dataIsShown ? <CountryComponentDisplay key={countryData.name} countryData={countryData} /> : <span />}
     </span>
   );
 }
@@ -46,9 +82,10 @@ const CountriesDisplay = ({ allCountriesData, countryQuery }) => {
     <div>
       {nrOfCountries > 10 ? <p>Too many matches, be more specific</p>
         : nrOfCountries <= 10 && nrOfCountries > 1 ? countryFilterList
-          .map(countryData => <div>{countryData.name}<ShowDataButtonDisplay countryData={countryData} /></div>)
-        : nrOfCountries === 1 ? <CountryComponentDisplay countryData={countryFilterList[0]} />
-        : <IAmErrorComponent />
+          .map(countryData => <div key={countryData.name}>{countryData.name}
+            <ShowDataButtonDisplay countryData={countryData} /></div>)
+          : nrOfCountries === 1 ? <CountryComponentDisplay countryData={countryFilterList[0]} />
+            : <IAmErrorComponent />
       }
     </div>
   )
